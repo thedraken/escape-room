3772e52074ac9dc3bfbcae5c916052482356244b
draken <thedraken@gmail.com>
2025-10-02 16:12:42 +0200

    Initial file structure to share with team-mates

---
+++ escaperoom/rooms/base.py
@@ -0,0 +1,13 @@
+from abc import abstractmethod, ABC
+
+
+class BaseRoom(ABC):
+    @abstractmethod
+    def __init__(self):
+        while False:
+            yield none
+
+    @abstractmethod
+    def solve(self):
+        while False:
+            yield none
