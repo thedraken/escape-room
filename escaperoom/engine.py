d297db9e6a0f6a67d67265a77d055746f689f461
draken <thedraken@gmail.com>
2025-10-09 14:12:07 +0200

    Can move between rooms

--- escaperoom/engine.py
+++ escaperoom/engine.py
@@ -1,9 +1,9 @@
 from enum import Enum
 class Engine:
     def __init__(self):
-        current_location = CurrentRoom.BASE
+        self.current_location = CurrentRoom.BASE
     def command(self, command):
-        match command:
+        match command.lower():
             case "quit":
                 print("Thank you for playing")
                 return False
@@ -21,11 +21,40 @@ class Engine:
                     "load: If a save.txt file is found and it is valid, will load it and set that as your current progress. Will overwrite any progress already done in this session")
                 print("quit: Exits the game and prints currently collected evidence to a transcript file")
                 print("help: Gives a list of available commands")
+            case move if move.startswith("move"):
+                is_valid = False
+                commands = move.split(" ")
+                if commands[0] == "move" and len(commands) > 1:
+                    new_room = commands[1]
+                    match new_room.lower():
+                        case "dns":
+                            self.current_location = CurrentRoom.DNS
+                            is_valid = True
+                        case "malware":
+                            self.current_location = CurrentRoom.MALWARE
+                            is_valid = True
+                        case "soc":
+                            self.current_location = CurrentRoom.SOC
+                            is_valid = True
+                        case "vault":
+                            self.current_location = CurrentRoom.VAULT
+                            is_valid = True
+                        case "gate":
+                            self.current_location = CurrentRoom.FINAL_GATE
+                            is_valid = True
+                        case "lobby":
+                            self.current_location = CurrentRoom.BASE
+                            is_valid = True
+                if not is_valid:
+                    print("Please enter a room with the move command, possible rooms are dns, malware, soc, vault, gate, and lobby E.g. move dns")
+                else:
+                    print(f"You have entered into {self.current_location}")
         return True
 class CurrentRoom(Enum):
     BASE = 1
     SOC = 2
     DNS = 3
     VAULT = 4
-    FINAL_GATE = 5
+    MALWARE = 5
+    FINAL_GATE = 6
 
