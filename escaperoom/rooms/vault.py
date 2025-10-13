ac5ebf7256dadcb4a68c71b4f4f0bd9b7574cb2f
thomas.mortimer <thedraken@gmail.com>
2025-10-13 22:45:25 +0200

    Initial implementation of the vault to find the values

--- escaperoom/rooms/vault.py
+++ escaperoom/rooms/vault.py
@@ -1,3 +1,5 @@
+import re
+
 from escaperoom.rooms.base import BaseRoom
 from escaperoom.rooms.currentroom import CurrentRoom
 from escaperoom.transcript import Transcript
@@ -13,5 +15,12 @@ class VaultRoom(BaseRoom):
         Add a method description here. Do not forget to return the result!
         :return:
         """
+
+        # https://regex101.com/r/AKN3hE/2
+        p = re.compile(pattern="\s*S\s*A\s*F\s*E\s*\{\s*(\d+)\s*-\s*(\d+)\s*-\s*(\d+)\s*}\s*",
+                       flags=re.MULTILINE | re.IGNORECASE)
+        # As I have three matching groups, will return a tuple of 3 items
+        # TODO TM Open file and get data
+        tuple_result = p.findall("")
         self.transcript.print_message("You called solve on " + CurrentRoom.get_room_name(self.__room))
         self._add_log_to_transcript("I did something", self.__room)
