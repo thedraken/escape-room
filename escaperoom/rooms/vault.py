c709dec51d9e5eb08920c4a3a176fa4bcbfe17f3
draken <thedraken@gmail.com>
2025-10-14 08:33:14 +0200

    Vault read file and regex match the data into a result tuple

--- escaperoom/rooms/vault.py
+++ escaperoom/rooms/vault.py
@@ -15,12 +15,17 @@ class VaultRoom(BaseRoom):
         Add a method description here. Do not forget to return the result!
         :return:
         """
-
-        # https://regex101.com/r/AKN3hE/2
-        p = re.compile(pattern="\s*S\s*A\s*F\s*E\s*\{\s*(\d+)\s*-\s*(\d+)\s*-\s*(\d+)\s*}\s*",
-                       flags=re.MULTILINE | re.IGNORECASE)
-        # As I have three matching groups, will return a tuple of 3 items
-        # TODO TM Open file and get data
-        tuple_result = p.findall("")
-        self.transcript.print_message("You called solve on " + CurrentRoom.get_room_name(self.__room))
-        self._add_log_to_transcript("I did something", self.__room)
+        try:
+            with open("data\\vault_dump.txt", "r") as vault_file:
+                lines = vault_file.readlines()
+                p = re.compile(pattern="\s*S\s*A\s*F\s*E\s*\{\s*(\d+)\s*-\s*(\d+)\s*-\s*(\d+)\s*}\s*",
+                               flags=re.MULTILINE | re.IGNORECASE)
+                for line in lines:
+                    # https://regex101.com/r/AKN3hE/2
+                    # As I have three matching groups, will return a tuple of 3 items
+                    # TODO TM Open file and get data
+                    tuple_result = p.findall(line)
+                    self.transcript.print_message("You called solve on " + CurrentRoom.get_room_name(self.__room))
+                    self._add_log_to_transcript("I did something", self.__room)
+        except Exception as e:
+            self.transcript.print_message("An error occurred:\n" + str(e))
