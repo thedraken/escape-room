1462789081281295a9a05ef30a5dbca78dae2773
draken <thedraken@gmail.com>
2025-10-16 10:15:18 +0200

    Save and load current state to a save.json file

--- escaperoom/utils.py
+++ escaperoom/utils.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from escaperoom.transcript import Transcript
 
 
@@ -7,19 +10,40 @@ class Utils:
 
     def save(self):
         """
-
+        Saves the current progress of the game to a save.json file
         :return: Returns true if it successfully saved the current state
         """
-        self.transcript.print_message("Save")
-        return True
+        self.transcript.print_message("Saving progress...")
+        try:
+            transcript_dict = self.transcript.transcript_dict
+            new_dict = []
+            for item in transcript_dict:
+                string_key = str(item)
+                new_dict.append([string_key, transcript_dict[item]])
+            with open(os.sep.join(["data", "save.json"]), "w") as save_file:
+                save_file.write(json.dumps(new_dict))
+            self.transcript.print_message("Progress saved.")
+            return True
+        except Exception as e:
+            self.transcript.print_message("Error saving progress: " + str(e))
+        return False
 
     def load(self):
         """
-
+        Loads the current state from the save.json file, if it exists
+        Will overwrite the current state, with the data
         :return: Returns true if it successfully loaded the save file
         """
-        self.transcript.print_message("Load")
-        return True
+        self.transcript.print_message("Loading progress...")
+        try:
+            with open(os.sep.join(["data", "save.json"]), "r") as save_file:
+                json_file = json.load(save_file)
+                self.transcript.transcript_dict = json_file
+                return True
+        except Exception as e:
+            self.transcript.print_message("Error loading save file: " + str(e))
+        return False
+
 
     def convert_to_float(self, value: str) -> float | None:
         """
