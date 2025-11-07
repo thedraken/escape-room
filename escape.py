26cffd02e0511ced3f26c44a05e7eda36825fea1
draken <thedraken@gmail.com>
2025-11-07 10:12:43 +0100

    Due to pylint behaving differently on GitHub and my device, changing the escape.py class to use a class to run the code.

--- escape.py
+++ escape.py
@@ -2,10 +2,94 @@
 Main escape room module, executes the game and creates the base classes
 """
 import argparse
-
+from dataclasses import dataclass
 from escaperoom.engine import Engine
 from escaperoom.transcript import Transcript
 from escaperoom.utils import Inventory, Utils
+
+
+@dataclass
+class Escape:
+    """
+    The main escape room class that launches the game, one of these will be
+    created and parse the arguments and then run the game
+    """
+    def __init__(self, arguments):
+        self.__args__ = arguments
+
+    def run_game(self):
+        """
+        Uses the arguments provided to run the game, if invalid will stop
+        the game
+        """
+        data_folder_location = self.__args__.data_folder_location
+        if data_folder_location is not None:
+            data_folder_location = data_folder_location.lower().strip()
+        if data_folder_location is None or data_folder_location == "":
+            data_folder_location = "data"
+
+        save_file_name = args.transcript
+        if save_file_name is not None:
+            save_file_name = save_file_name.lower().strip()
+        if save_file_name is None or save_file_name == "":
+            save_file_name = "run.txt"
+
+        transcript = Transcript(data_folder_location)
+        inventory = Inventory(transcript)
+        utils = Utils(transcript, inventory, data_folder_location,
+                      save_file_name)
+        start_room = self.__args__.start
+        if start_room is not None:
+            start_room = start_room.lower().strip()
+
+        run_game = True
+        if start_room is None or start_room == "" or start_room == "intro":
+            transcript.print_message("Welcome to the escape room game")
+            transcript.print_message(r"""
+            ___________                                    __________                                                             
+            \_   _____/ ______ ____ _____  ______   ____   \______   \ ____   ____   _____                                        
+             |    __)_ /  ___// ___\\__  \ \____ \_/ __ \   |       _//  _ \ /  _ \ /     \                                       
+             |        \\___ \\  \___ / __ \|  |_> >  ___/   |    |   (  <_> |  <_> )  Y Y  \
+            /_______  /____  >\___  >____  /   __/ \___  >  |____|_  /\____/ \____/|__|_|  /                                      
+                    \/     \/     \/     \/|__|        \/          \/                    \/                                       
+                """)
+            transcript.print_message("Enjoy the escape room!")
+            transcript.print_message("Type quit to quit")
+            transcript.print_message("Type hint for assistance")
+
+        engine = Engine(transcript, inventory, utils)
+        auto_run = self.__args__.auto_run
+
+        while run_game:
+            if auto_run:
+                engine.command("move soc")
+                engine.command("inspect auth.log")
+                engine.command("move dns")
+                engine.command("inspect dns.cfg")
+                engine.command("move vault")
+                engine.command("inspect vault_dump.txt")
+                engine.command("move malware")
+                engine.command("inspect proc_tree.jsonl")
+                engine.command("move gate")
+                run_game = engine.command("use final_gate")
+                auto_run = False
+            else:
+                if start_room is not None and start_room != "":
+                    match start_room:
+                        case "dns":
+                            engine.command("move dns")
+                        case "malware":
+                            engine.command("move malware")
+                        case "soc":
+                            engine.command("move soc")
+                        case "vault":
+                            engine.command("move vault")
+                        case "gate":
+                            engine.command("move gate")
+
+                next_step = input("What would you like to do?")
+                transcript.append_log("What would you like to do?")
+                run_game = engine.command(next_step)
 
 parser = argparse.ArgumentParser(prog="Escape room",
                                  description="Group 9's escape room")
@@ -35,72 +119,4 @@ parser.add_argument("--data_folder_location",
                     default="data")
 
 args = parser.parse_args()
-
-DATA_FOLDER_LOCATION = args.data_folder_location
-if DATA_FOLDER_LOCATION is not None:
-    DATA_FOLDER_LOCATION = DATA_FOLDER_LOCATION.lower().strip()
-if DATA_FOLDER_LOCATION is None or DATA_FOLDER_LOCATION == "":
-    DATA_FOLDER_LOCATION = "data"
-
-SAVE_FILE_NAME = args.transcript
-if SAVE_FILE_NAME is not None:
-    SAVE_FILE_NAME = SAVE_FILE_NAME.lower().strip()
-if SAVE_FILE_NAME is None or SAVE_FILE_NAME == "":
-    SAVE_FILE_NAME = "run.txt"
-
-transcript = Transcript(DATA_FOLDER_LOCATION)
-inventory = Inventory(transcript)
-utils = Utils(transcript, inventory, DATA_FOLDER_LOCATION, SAVE_FILE_NAME)
-start_room = args.start
-if start_room is not None:
-    start_room = start_room.lower().strip()
-
-RUN_GAME = True
-if start_room is None or start_room == "" or start_room == "intro":
-    transcript.print_message("Welcome to the escape room game")
-    transcript.print_message(r"""
-    ___________                                    __________                                                             
-    \_   _____/ ______ ____ _____  ______   ____   \______   \ ____   ____   _____                                        
-     |    __)_ /  ___// ___\\__  \ \____ \_/ __ \   |       _//  _ \ /  _ \ /     \                                       
-     |        \\___ \\  \___ / __ \|  |_> >  ___/   |    |   (  <_> |  <_> )  Y Y  \
-    /_______  /____  >\___  >____  /   __/ \___  >  |____|_  /\____/ \____/|__|_|  /                                      
-            \/     \/     \/     \/|__|        \/          \/                    \/                                       
-        """)
-    transcript.print_message("Enjoy the escape room!")
-    transcript.print_message("Type quit to quit")
-    transcript.print_message("Type hint for assistance")
-
-engine = Engine(transcript, inventory, utils)
-AUTO_RUN = args.auto_run
-
-
-while RUN_GAME:
-    if AUTO_RUN:
-        engine.command("move soc")
-        engine.command("inspect auth.log")
-        engine.command("move dns")
-        engine.command("inspect dns.cfg")
-        engine.command("move vault")
-        engine.command("inspect vault_dump.txt")
-        engine.command("move malware")
-        engine.command("inspect proc_tree.jsonl")
-        engine.command("move gate")
-        RUN_GAME = engine.command("use final_gate")
-        AUTO_RUN = False
-    else:
-        if start_room is not None and start_room != "":
-            match start_room:
-                case "dns":
-                    engine.command("move dns")
-                case "malware":
-                    engine.command("move malware")
-                case "soc":
-                    engine.command("move soc")
-                case "vault":
-                    engine.command("move vault")
-                case "gate":
-                    engine.command("move gate")
-
-        next_step = input("What would you like to do?")
-        transcript.append_log("What would you like to do?")
-        RUN_GAME = engine.command(next_step)
+escape = Escape(args)
